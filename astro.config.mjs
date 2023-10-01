1c5847c2803c4c91e8942d654c16f9cea772d377
admin <admin@MacBook-Pro-de-admin-2.local>
2023-10-01 01:40:41 -0700

    cname public

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site: 'https://tecuan.net/',
+  site: 'https://www.tecuan.net/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
